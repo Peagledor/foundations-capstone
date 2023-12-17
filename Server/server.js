5d4397ff11d1d8aeb75e608de5b49ceeb4043012
Jorge Peraza <j.peraza.22@gmail.com>
2023-12-16 22:19:36 -0700

    Adds working API calls. working display frontent, needs styling and refining

--- Server/server.js
+++ Server/server.js
@@ -1,121 +1,54 @@
-// require('dotenv').config();
 const SERVER_PORT = 4004;
 const express = require('express');
 const axios = require('axios');
 const app = express();
 const cors = require('cors');
-const path = require('path');
+// const path = require('path');
 
 
 const KEY = '662a129c5b90d8e37ab11bad63eab67f';
-const KEY_TWO = 'abe81164b6408f7f78955541dba4dd84'
-// const url = `http://api.openweathermap.org/geo/1.0/direct?q=${params.city}, ${params.state}&limit=5&appid=${KEY}`;
+// const KEY_TWO = 'abe81164b6408f7f78955541dba4dd84'
+// const url = `http://api.openweathermap.org/geo/1.0/direct?q=${`params.city`}, ${`params.state`}&limit=5&appid=${KEY}`;
 // const weatherURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${`lat`}&lon=${`lon`}&exclude=${`exclude`}&appid=${KEY_TWO}`;
 
 // const fns = require('./controller');
 
+// app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.post('/api/weather', (req, res) => {
+    ///////// need to make reusability work below///////////
+    // const url = 'http://api.openweathermap.org/geo/1.0/zip?zip=80205&appid=abe81164b6408f7f78955541dba4dd84' 
     const params = req.body;
         
-    console.log("server side:", params.city, params.state);
+    console.log("server side:", params);
 
-    axios.get(url, {
-        params: {
-            city: params.city,
-            state: params.state
-        }
-    }
-    )
+    axios.get('http://api.openweathermap.org/geo/1.0/zip?zip=80205&appid=abe81164b6408f7f78955541dba4dd84')
     .then( response => {
-        const lat = response.data.lat;
-        const lon = response.data.lon;
+        console.log("first response:", response.data);
+        const data = response.data;
+        const lat = data.lat;
+        const lon = data.lon;
         const exclude = `minutely,hourly,daily,alerts`;
-        
-        axios.get(weatherURL, {
-            params: {
-                lat,
-                lon,
-                exclude,
-                KEY
-            }
-        })
-        .then(weatherResponse => {
-                console.log("second axios request:", params)
+        console.log(`coords:`, lat , lon)
 
-                console.log(weatherResponse);
-                
-                const currentWeather = weatherResponse.data;
-                
-                console.log(currentWeather);
-                res.send(currentWeather);
-            }) 
-        .catch(err => {
-        res.status(500).send('Internal Server Error')})
-})})
+    }) 
+    axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=39.7392364&lon=-104.984862&exclude=minutely,hourly,daily,alerts&appid=abe81164b6408f7f78955541dba4dd84`)
+    .then(weatherRes => {
+        console.log("weather response:", weatherRes.data)
 
-// const lat = response.lat
-// const lon = response.lon
-// const exclude = `minutely, hourly, daily, alerts`
-// axios.get(weatherURL, res)
-// .then(res => {
-//     const currentWeather = res.data
-//     console.log(currentWeather)
-//     res.send(currentWeather)
-// })
+        const currentWeather = {
+            current: weatherRes.data.current
+        }
 
+        console.log('current weather:', currentWeather);
+        res.send(currentWeather);
+    }
+)
+    .catch(err => {
+    res.status(500).send('Internal Server Error')})
+})
 
-// app.post('/api/weather', (req, res) => {
-//     const params = req.body;
-        
-//     console.log("server side:", params.city, params.state);
-
-//     axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${params.city}, ${params.state}&limit=5&appid=${KEY}`, {
-//         params: {
-//             city: params.city,
-//             state: params.state
-//         }
-//     })
-//     .then(response => {
-
-//         console.log("first response:", response);
-
-//         if(response.dara.lat && response.data.lon) {
-//             const lat = response.data[0].lat;
-//             const lon = response.data[0].lon;
-//             const exclude = `minutely,hourly,daily,alerts`;
-
-        
-//         axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${KEY_TWO}`, {
-//             // params: {
-//             //     lat: params.lat,
-//             //     lon: params.lon,
-//             //     exclude,
-//             //     KEY
-//             // }
-//         })
-//         // console.log(params)
-//         .then(weatherResponse => {
-//             console.log("second API response:", weatherResponse);
-                        
-//             const currentWeather = weatherResponse.data.value;
-//             console.log("current weather", currentWeather);
-//             res.send(currentWeather);
-//         })
-//         .catch(err => {
-//             console.error("Error in second Axios request:", err);
-//             res.status(500).send('Internal Server Error');
-//         })
-//         } else {
-//             res.status(500).send('Internal Server Error')
-//     }})
-//     .catch(err => {
-//         // console.error("Error in first Axios request:", err);
-//         res.status(500).send('Internal Server Error');
-//     });
-// });
 
 app.listen(SERVER_PORT, () => console.log(`Running on ${SERVER_PORT}`))
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const cityInput = document.querySelector('#city-input');
-const stateInput = document.querySelector('#state-input')
-const weatherCard = document.querySelector('.card-container')
+const zipInput = document.querySelector('#zip-input');
+const weatherCard = document.querySelector('.card-container');
 const form = document.querySelector('form');
 
 const weatherURL = `http://localhost:4004/api/weather`;
@@ -8,33 +7,44 @@ const weatherURL = `http://localhost:4004/api/weather`;
 const submitHndlr = event => {
     event.preventDefault();
 
-    if (!cityInput.value) {
-        alert('you must enter a city name')
+    if (!zipInput.value) {
+        alert('you must enter a zip code')
         return;
     } 
 
     let body = {
-        city: cityInput.value,
-        state: stateInput.value
+        zipCode: zipInput.value,
     }
 
     axios.post(`${weatherURL}`, body)
     .then(response => {
-        displayWeather(response.data);
+        console.log("received response:", response)
+        const weatherData = response.data.current;
+
+        displayWeather(weatherData);
     })
     .catch(error => {
         console.error('Error:', error);
     });
 }
 
-const displayWeather = data => {
-    const cardContainer = document.querySelector('.card-container');
-    const weatherCard = document.createElement('div');
+const displayWeather = data => { // takes in response data
+    const cardContainer = document.querySelector('.card-container'); // selects html by class
+    const weatherCard = document.createElement('div'); // creates html dive inside card container
+    const weatherData = data; // assigns data to weatherData variable
+    console.log("passed in data:", weatherData) // logs 
 
-    weatherCard.innerHTML = `<p>front end code works. just needs to display the already deconstructed weather data from back end.</p>`
-    cardContainer.appendChild(weatherCard);
+    //Need to make function that selects specific data to display. probably outside of this one 
     
-    console.log(data);
+    const dataStr = JSON.stringify(weatherData) //takes in the passed in weather data and converts it to a string
+    console.log("data string:", dataStr) // logs
+
+    
+
+    weatherCard.innerHTML = `<p>${dataStr}</p>` // turns data string to html string
+
+    cardContainer.appendChild(weatherCard); // creates weather card iside the card container
+    
 }
 
 form.addEventListener('submit', submitHndlr)
